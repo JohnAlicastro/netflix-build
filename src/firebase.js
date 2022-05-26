8751ed046b74d81f10c1e7e281649707b0ee4c8a
John Alicastro <93687275+JohnAlicastro@users.noreply.github.com>
2022-05-26 17:28:28 -0400

    Add project to github

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyDl0_Fn6kMX-xz70uTi15e3Bqk2vtVLou8',
+  authDomain: 'netflix-build-78671.firebaseapp.com',
+  projectId: 'netflix-build-78671',
+  storageBucket: 'netflix-build-78671.appspot.com',
+  messagingSenderId: '404287569982',
+  appId: '1:404287569982:web:047778d167e9eda996116f',
+};
