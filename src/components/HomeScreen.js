359e5709f8c21199bbe346271a6c2b77484e7b49
John Alicastro <93687275+JohnAlicastro@users.noreply.github.com>
2022-05-27 17:11:10 -0400

    Banner WIP

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,37 +1,14 @@
 import React from 'react';
 import './HomeScreen.css';
 import Nav from './Nav';
+import Banner from './Banner';
 
 const HomeScreen = () => {
   return (
     <div className='homeScreen'>
       <Nav />
 
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-
-      {/* Banner */}
+      <Banner />
 
       {/* Row */}
     </div>
