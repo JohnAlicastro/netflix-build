197484c7a9d83a6c21fde6eafee95a3e3d6b3982
John Alicastro <93687275+JohnAlicastro@users.noreply.github.com>
2022-06-03 17:02:36 -0400

    Implement react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import HomeScreen from './components/HomeScreen';
 
@@ -6,7 +7,13 @@ function App() {
   /* RETURN RENDER */
   return (
     <div className='app'>
-      <HomeScreen />
+      {/* <HomeScreen /> */}
+
+      <Router>
+        <Routes>
+          <Route path='/' element={<HomeScreen />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
