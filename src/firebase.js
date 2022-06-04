92195bbb229c35585c4b1981cd6a23135397e062
John Alicastro <93687275+JohnAlicastro@users.noreply.github.com>
2022-06-03 20:10:52 -0400

    WIP firebase auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,9 @@
+// import firebase from 'firebase';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore/lite';
+import { getAuth } from 'firebase/auth';
+
+/* FIREBASE CONFIGURATION KEY */
 const firebaseConfig = {
   apiKey: 'AIzaSyDl0_Fn6kMX-xz70uTi15e3Bqk2vtVLou8',
   authDomain: 'netflix-build-78671.firebaseapp.com',
@@ -6,3 +12,17 @@ const firebaseConfig = {
   messagingSenderId: '404287569982',
   appId: '1:404287569982:web:047778d167e9eda996116f',
 };
+
+/* INITIALIZE FIREBASE APPLICATION */
+/* commented our lines below are from older version of firebase */
+
+// const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
+// const db = firebaseApp.firestore();
+const db = getFirestore(firebaseApp);
+// const auth = firebase.auth();
+const auth = getAuth(firebaseApp);
+
+/* EXPORTS */
+export { auth };
+export default db;
--- src/screens/SingupScreen.js
+++ src/screens/SingupScreen.js
@@ -1,21 +1,49 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { auth } from '../firebase';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import './SingupScreen.css';
 
 /* SIGNUP SCREEN COMPONENT */
 const SingupScreen = () => {
+  /* REF (STATE) - EMAIL AND PASSWORD FROM INPUT FORM BELOW */
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  /* REGISTER FUNC */
+  const register = (e) => {
+    e.preventDefault();
+
+    createUserWithEmailAndPassword(auth, emailRef.current.value, passwordRef.current.value)
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
+  /* SIGNIN FUNC */
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+
   /* RETURN RENDER */
   return (
     <div className='singupScreen'>
       <form>
         <h1>Sign In</h1>
 
-        <input type='email' placeholder='Email' />
-        <input type='password' placeholder='Password' />
-        <button type='submit'>Sign In</button>
+        <input ref={emailRef} type='email' placeholder='Email' />
+        <input ref={passwordRef} type='password' placeholder='Password' />
+        <button type='submit' onClick={signIn}>
+          Sign In
+        </button>
 
         <h4>
           <span className='signupScreen__gray'>New to Netflix? </span>
-          <span className='signupScreen__link'>Sign Up now.</span>
+          <span className='signupScreen__link' onClick={register}>
+            Sign Up now.
+          </span>
         </h4>
       </form>
     </div>
