d54214684a9495cd89c6a634b7d6444ad1d64a95
John Alicastro <93687275+JohnAlicastro@users.noreply.github.com>
2022-06-07 19:53:52 -0400

    Add Redux user selector and dispatch login/logout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
 // IMPORTING auth FROM LOCAL firebase.js FILE IN src FOLDER //
 import { auth } from './firebase';
 import { onAuthStateChanged } from 'firebase/auth';
 import './App.css';
-import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
 
+/* APP COMPONENT */
 function App() {
-  // TEMP USER VARIABLE USED FOR RETURN RENDER ROUTER BELOW//
-  const user = null;
+  /* TEMP USER VARIABLE USED FOR RETURN RENDER ROUTER BELOW */
+  // const user = null;
 
-  /* EFFECT - TRACKING IF USER IS LOGGED IN OR NOT */
+  /* USER VARIABLE GETTING user FROM REDUX STORE, USED FOR RETURN RENDER ROUTER BELOW */
+  const user = useSelector(selectUser);
+
+  /* dispatch VARIABLE TO INVOKE useDispatch FROM REDUX  */
+  const dispatch = useDispatch();
+
+  /* EFFECT - TRACKING IF USER IS LOGGED IN OR NOT & DISPATCHING ACTIONS FROM REDUX */
   useEffect(() => {
     // onAuthStateChanged IS AN EVENT LISTENER FUNC STORED IN THE BROWSER //
     const unsubscribe = onAuthStateChanged(auth, (userAuth) => {
       if (userAuth) {
-        // LOGGED IN //
-        console.log(userAuth);
+        // LOGGED IN
+        // console.log(userAuth);
+        /* DISPATCH LOGIN ACTION FROM REDUX */
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
       } else {
-        // LOGGED OUT //
+        // LOGGED OUT
+        /* DISPATCH LOGOUT ACTION FROM REDUX */
+        dispatch(logout);
       }
     });
 
