9f2869db60504e9da4637f459e72479f2a9e93d4
John Alicastro <93687275+JohnAlicastro@users.noreply.github.com>
2022-06-10 16:23:07 -0400

    WIP ProfileScreen styling 2

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -10,6 +10,9 @@ const ProfileScreen = () => {
       <Nav />
       <div className='profileScreen__body'>
         <h1>Edit Profile</h1>
+        <div className='profileScreen__info'>
+          <img src='https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png' alt='' />
+        </div>
       </div>
     </div>
   );
