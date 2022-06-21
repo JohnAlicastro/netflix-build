d05033ff8b5eea5b02daf22a0c1f39062c697e4c
John Alicastro <93687275+JohnAlicastro@users.noreply.github.com>
2022-06-21 16:51:28 -0400

    LoginScreen login test 1

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -12,9 +12,9 @@ const LoginScreen = () => {
     <div className='loginScreen'>
       <div className='loginScreen__background'>
         <img className='loginScreen__logo' src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png' alt='' />
-        <button className='loginScreen__button' onClick={() => setSignIn(true)}>
+        {/* <button className='loginScreen__button' onClick={() => setSignIn(true)}>
           Sign In
-        </button>
+        </button> */}
         <div className='loginScreen__gradient' />
       </div>
 
@@ -28,12 +28,12 @@ const LoginScreen = () => {
             <h2>Watch anywhere. Cancel at any time.</h2>
             <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
             <div className='loginScreen__input'>
-              <form>
+              {/* <form>
                 <input type='email' placeholder='Email Address' />
                 <button className='loginScreen__getStarted' onClick={() => setSignIn(true)}>
                   GET STARTED
                 </button>
-              </form>
+              </form> */}
             </div>
           </>
         )}
