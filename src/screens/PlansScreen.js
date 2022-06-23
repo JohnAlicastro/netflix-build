9bb103339b0fd7ebd61985be5b9f2199d7811c9a
John Alicastro <93687275+JohnAlicastro@users.noreply.github.com>
2022-06-23 16:33:21 -0400

    WIP PlansScreen 2

--- src/screens/PlansScreen.js
+++ src/screens/PlansScreen.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+// IMPORTING db FROM LOCAL firebase.js FILE IN src FOLDER //
+import db from '../firebase';
+import './PlansScreen.css';
 
+/* PLANSSCREEN COMPONENT */
 const PlansScreen = () => {
-  return <div>PlansScreen</div>;
+  /* STATE - ACTIVE PRODUCT (PLAN) */
+  const [products, setProducts] = useState([]);
+
+  /* EFFECT - PULL PRODUCT (PLAN) INFO FROM DB ONCE ON MOUNT */
+  useEffect(() => {
+    db.collection('products')
+      .where('active', '==', 'true')
+      .get()
+      .then((queySnapshot) => {
+        const products = {};
+        querySnapshot.forEach(async (productDoc) => {
+          products[productDoc.id] = productDoc.data();
+          const priceSnap = await productDoc.ref.collection('prices').get();
+          priceSnap.docs.forEach((price) => {
+            products[productDoc.id].prices = {
+              priceId: price.id,
+              priceData: price.data,
+            };
+          });
+        });
+        setProducts(products);
+      });
+  }, []);
+
+  /* RETURN RENDER */
+  return <div className='PlansScreen'>PlansScreen</div>;
 };
 
 export default PlansScreen;
